1acf6112dcc3eb2be84cc9f5eee7c782d4526d87
Guillaume Bouyer <guillaume.bouyer@me.com>
2022-06-08 14:32:29 +0200

    Rand date in fixtures

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -14,11 +14,15 @@ class ArticleFixtures extends Fixture
         // Boucle for() pour insérer un certains nombre de données
         for($i = 0; $i < 10; $i++) {
 
+            $number = rand(1, 500);
+            $date = new DateTimeImmutable();
+            $randDate = $date->modify("+$number day");
+
             // Instancie l'entité avec laquelle travailler
             $article = new Article();
             $article->setTitle("Titre_$i");
             $article->setDescription("Description_$i");
-            $article->setCreatedAt(new DateTimeImmutable());
+            $article->setCreatedAt($randDate);
 
             // Met de côté les données en attente d'insertion
             $manager->persist($article);

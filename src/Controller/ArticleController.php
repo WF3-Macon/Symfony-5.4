212b4461b2e9132b2265de01550a637c05fd1c85
Guillaume Bouyer <guillaume.bouyer@me.com>
2022-06-08 12:04:09 +0200

    Selection en BDD

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
+use DateTimeImmutable;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/article', name: 'app_article')]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+            'article' => $articleRepository->find(4),
+            'post' => $articleRepository->findOneBy(['title' => 'Titre_7'], ['created_at' => 'DESC']),
+            'posts' => $articleRepository->findBy(['created_at' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-06-08 08:23:03')])
+        ]);
+    }
+}
